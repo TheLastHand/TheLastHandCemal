948846759b2744450722bf0c9856365a1e522c0c
cemaldogukan <cemal.dogukan@ozu.edu.tr>
2015-04-04 00:12:00 +0300

    Create Taunt.java

---
+++ Taunt.java
@@ -0,0 +1,22 @@
+
+public class Taunt implements Card{
+
+	@Override
+	public void attack(int attackPower) {
+		attackPower = 5;
+		
+	}
+
+	@Override
+	public void defense(int defensePower) {
+		defensePower = 3;
+	}
+
+	@Override
+	public void getImage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
